af410f57f90a4a8e4e0d172a5cf46ef5e1295c99
Sam Duan <dsqchn@gmail.com>
2020-04-03 20:10:14 -0400

    added a simple tester

---
+++ tester.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+import subprocess
+import time
+import datetime
+
+import weatherHandler
+import displayHandler
+
+import RPi.GPIO as GPIO
+
+display = displayHandler.SerialOLEDDisplay(128, 64)
+display.add_string(0, 0, maxX=32, maxY=32, string="Good day fellers this is a line", fontSize=17)
+display.display_everything()
